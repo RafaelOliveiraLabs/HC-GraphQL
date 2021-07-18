b6884b1160e50b75936e145cb9e569c0b23feb25
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-18 13:48:04 -0300

    reqAssync

--- Projetos/dev-demands/packages/web/src/pages/SignIn.js
+++ Projetos/dev-demands/packages/web/src/pages/SignIn.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://127.0.0.1:8000/authenticate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((response) => response.JSON())
+      .then(() => {
+        console.log("Success");
+      });
+  };
+
+  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
+
   return (
     <>
-      <form action="/authenticate" method="POST">
+      <form onSubmit={handleSubmit}>
         <fieldset>
-          <label for="email">Email</label>
-          <input id="email" name="email" type="email" inputmode="email" autocomplete="username" />
+          <label htmlFor="email">Email</label>
+          <input id="email" value={email} onChange={handleEmailChange} type="email" inputMode="email" autoComplete="username" />
         </fieldset>
+
         <fieldset>
-          <label for="password">Senha</label>
-          <input id="password" name="password" type="password" autocomplete="current-username" />
+          <label htmlFor="password">Senha</label>
+          <input id="password" value={password} onChange={handlePasswordChange} type="password" autoComplete="current-username" />
         </fieldset>
         <button type="submit">Entrar</button>
       </form>
